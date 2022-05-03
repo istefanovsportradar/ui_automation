3e619f0cc6b8ff3980eb9bcd1e89d2c61bab32ea
istefanovsportradar <i.stefanov.ext@sportradar.com>
2022-05-03 15:40:27 -0600

    login page changes

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -1,4 +1,4 @@
-import { ChainablePromiseElement } from 'webdriverio';
+import HelperPage from './helper.page';
 
 import Page from './page';
 
@@ -6,51 +6,54 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class LoginPage extends Page {
-    /**
+     /**
+      * define selectors using getter methods
+      */
+     public get inputUsername() {
+          return '#username'
+     }
+     /**
      * define selectors using getter methods
      */
-    public get inputUsername()  {
-         return '#username'
-    }
-    /**
-    * define selectors using getter methods
-    */
-    public get inputPassword()  {
-         return '#password'
-    }
-    /**
-        * define selectors using getter methods
-        */
-    public get btnSubmit()  {
-         return 'button[type="submit"]'
-    }
-    /**
-    * define selectors using getter methods
-    */
-    public get inputUsername_Ig()  {
-         return 'input[placeholder="User Name"]'
-    }
-    /**
-    * define selectors using getter methods
-    */
-     public get inputPassword_Ig()  {
-         return 'input[placeholder="Password"]'
-    }
-
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
+     public get inputPassword() {
+          return '#password'
+     }
+     /**
+         * define selectors using getter methods
+         */
+     public get btnSubmit() {
+          return 'button[type="submit"]'
+     }
+     /**
+     * define selectors using getter methods
      */
-    public async login(usernameElement, passwordElement, submitButton, username: string, password: string) {
-        const usernameField = await usernameElement
-        await usernameField.setValue(username)
+     public get inputUsername_Ig() {
+          return 'input[placeholder="User Name"]'
+     }
+     /**
+     * define selectors using getter methods
+     */
+     public get inputPassword_Ig() {
+          return 'input[placeholder="Password"]'
+     }
 
-        const passwordField = await passwordElement
-        await passwordField.setValue(password)
+     /**
+      * a method to encapsule automation code to interact with the page
+      * e.g. to login using username and password
+      */
+     public async login(usernameElement, passwordElement, submitButton, username: string, password: string) {
+          const usernameField = await usernameElement
+          await HelperPage.waitUntilElement(usernameField, 60000)
+          await usernameField.setValue(username)
 
-        const btnSubmit = await submitButton
-        await btnSubmit.click()
-    }
+          const passwordField = await passwordElement
+          await HelperPage.waitUntilElement(passwordField, 60000)
+          await passwordField.setValue(password)
+
+          const btnSubmit = await submitButton
+          await HelperPage.waitUntilElement(btnSubmit, 60000)
+          await btnSubmit.click()
+     }
 }
 
 export default new LoginPage();
